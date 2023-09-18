d0ed45eab60e5cc6b3b3634a30811366b22e96b1
Lisandro Ferreira <lisandroferrreira06@gmail.com>
2023-09-18 15:35:04 -0300

    Componente paginación

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,13 @@
-import React, { useEffect, useState } from "react";
-import CardBody from "./components/CardBody";
+import { useState } from 'react'
+import { ProductList } from './components/ProductList';
+function App() {
 
-import "./App.css";
-const App = () => {
-  const [items, setItem] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [addedItems, setAddedItem] = useState([]);
-  const [showAddProducts, setShowAddProducts] = useState(false);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products/")
-      .then((res) => res.json())
-      .then((data) => setItem(data));
-    console.count("hi");
-  }, []);
-  function changingSrarchData(e) {
-    setSearchValue(e.target.value);
-  }
-  const itmesFilter = items.filter((item) =>
-    item.title.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
-  function addItem(item) {
-    item.addNumber = 1;
-    const itemArr = addedItems;
-    setAddedItem([...itemArr, item]);
-  }
-  // console.log(addedItems);
-  function removeItem(item) {
-    const newItems = addedItems.filter((addedItem) => addedItem.id !== item.id);
-    setAddedItem(newItems);
-    // console.log(addedItems);
-  }
   return (
-    <div className="container">
-        <CardBody
-          products={itmesFilter}
-          addItem={addItem}
-          removeItem={removeItem}
-          addedItems={addedItems}
-        />
+    <div className='App'>
+      <h1 className='title'>Tienda</h1>
+      <ProductList />
     </div>
   );
-};
+}
 
-export default App;
+export default App
